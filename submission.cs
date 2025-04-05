5dbdf5f9a8412626ad40ea216b1689077dc6cb7c
gfiedler2026 <gfiedler@asu.edu>
2025-04-04 18:29:49 -0700

    Update submission.cs

--- submission.cs
+++ submission.cs
@@ -19,9 +19,9 @@ namespace ConsoleApp1
 
     public class Program
     {
-        public static string xmlURL = "Your XML URL";
-        public static string xmlErrorURL = "Your Error XML URL";
-        public static string xsdURL = "Your XSD URL";
+        public static string xmlURL = "https://github.com/gfiedler2026/cse445_assignment4/blob/main/Hotels.xml";
+        public static string xmlErrorURL = "https://github.com/gfiedler2026/cse445_assignment4/blob/main/HotelsErrors.xml";
+        public static string xsdURL = "https://github.com/gfiedler2026/cse445_assignment4/blob/main/Hotels.xsd";
 
         public static void Main(string[] args)
         {
@@ -40,18 +40,56 @@ namespace ConsoleApp1
         // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
+            string errorMessage = "No Error";
 
+            try
+            {
+                string xmlStuff = new WebClient().DownloadString(xmlUrl);
+                string xsdStuff = new WebClient().DownloadString(xsdUrl);
 
-            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+                XmlSchemaSet schema = XmlSchema.Read(xsdReader, null);
+                using (StringReader xsdReader = new StringReader(xsdStuff))
+                {
+                    XmlSchema schema = XmlSchema.Read(xsdReader, null);
+                    schemaSet.Add(schema);
+                }
+
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas = schemaSet;
+                settings.ValidationType = ValidationType.Schema;
+                settings.ValidationEventHandler += (sender, args) =>
+                {
+                    errorMessage = args.Message; 
+                }
+                using (StringReader xmlReader = new StringReader(xmlStuff))
+                using (xmlReader reader = xmlReader.Create(xmlReader, settings))
+                {
+                    while (reader.Reader()) { }
+                }
+            }
+            catch(Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+            return errorMessage;
         }
 
         public static string Xml2Json(string xmlUrl)
         {
-            
+            try
+            {
+                string xmlContent = new WebClient().DownloadString(xmlUrl);
+                XmlDocument doc  new XmlDocument();
+                doc.LoadXml(xmlContent);
+                string json = JsonConvert.SeralizeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+                return json;
+            }
+            catch(Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
 
             // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
-            return jsonText;
-
         }
     }
 
